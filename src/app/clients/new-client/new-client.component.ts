6dd53fc9b09ad9b8ed1c5412a8853c91bbffc3d4
João Pedro <Joape2004@gmail.com>
2025-03-18 18:12:54 -0300

    Criação do projeto

---
+++ src/app/clients/new-client/new-client.component.ts
@@ -0,0 +1,42 @@
+import { Component, Inject, OnDestroy } from '@angular/core';
+import { SERVICES_TOKEN } from '../../services/service.token';
+import { IClientService } from '../../services/api-client/clients/iclients.service';
+import { ClientsService } from '../../services/api-client/clients/clients.service';
+import { ClientFormComponent } from "../components/client-form/client-form.component";
+import { ClientModelForm } from '../client.models';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-new-client',
+  imports: [ClientFormComponent],
+  templateUrl: './new-client.component.html',
+  styleUrl: './new-client.component.scss',
+  providers: [
+    { provide: SERVICES_TOKEN.HTTP.CLIENT, useClass: ClientsService }
+  ]
+})
+export class NewClientComponent implements OnDestroy {
+
+  private httpSubscription?: Subscription
+
+  constructor(@
+    Inject(SERVICES_TOKEN.HTTP.CLIENT) private readonly httpServices: IClientService,
+    private readonly router: Router
+  ) { }
+
+
+  ngOnDestroy(): void {
+    if (this.httpSubscription) {
+      this.httpSubscription.unsubscribe()
+    }
+  }
+
+  onSubmitClient(value: ClientModelForm) {
+    const { id, ...request } = value
+    this.httpSubscription = this.httpServices.save(request).subscribe(_=> {
+      this.router.navigate(['clients/list'])
+    })
+  }
+
+}
